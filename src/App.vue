38150335cdba08879c17413f9c1fdc5fe95959d2
PraveenSingh6227 <vrcwebsolution@gmail.com>
2022-03-08 11:51:16 +0530

    second page movies detail

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,10 @@
 <template>
- 
-  <HelloWorld />
-  
-  <caRousel :slides="slides" :interval="3000" controls indicators></caRousel>
-  <Slider />
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Slider from './components/Slider.vue'
-import CaRousel from './components/carousel/CarouselName.vue'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld, CaRousel, Slider
-    
-  },
-  data: () => ({
-    slides: [
-      "https://assets-in.bmscdn.com/promotions/cms/creatives/1646035476610_sahweb.jpg",
-      "https://assets-in.bmscdn.com/promotions/cms/creatives/1645445632606_restaurant.jpg",
-      "https://assets-in.bmscdn.com/promotions/cms/creatives/1646307007376_martyrweb.jpg",
-      "https://assets-in.bmscdn.com/promotions/cms/creatives/1646301945227_kultekweb.jpg",
-      "https://assets-in.bmscdn.com/promotions/cms/creatives/1643608159306_fb.jpg",
-    ],
-  }),
-  
-};
-
-
-</script>
 
 <style>
 #app {
@@ -38,12 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
- margin-top:0px !important;
 }
-.carousel-inner{
-    position: relative;
-    width: 1400px;
-    height: 318px !important;
-    overflow: hidden;
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
